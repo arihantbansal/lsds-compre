33221c5bf06646cd9f8828a288c8228e4fc425c5
Akhil Bansal <bansalakhil19@gmail.com>
2023-05-04 21:01:19 +0530

    script to generate random write

---
+++ send_req.py
@@ -0,0 +1,12 @@
+import random
+import time
+
+def string_gen(size=20, chars='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+if __name__ == '__main__':
+    port = 21000
+    while True:
+        s = str(port) + '/write \'{"Set":{"key":'+ str(time.time()) +',"value":'+ string_gen() +'}}\''
+        print(s)
+        time.sleep(random.randint(1, 5))
